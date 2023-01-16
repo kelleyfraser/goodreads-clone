e3a6167d9e93edfb2083d79ce0455e713674b498
Kelley Fraser <kelleyirwinfraser@gmail.com>
2023-01-16 09:40:56 +0000

    Build news component.

---
+++ src/components/news.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function News() {
+  return (
+    <div className="section side-panel news-section">
+      <h1 className="section-heading">News & Interviews</h1>
+      <h2 className="article-title underlined-text">7 New Books Recommended by Readers This Week</h2>
+      <img src="./images/article.jpg"></img>
+      <p className="light-text">6 likes</p>
+    </div>
+  );
+}
+
+export default News;
+

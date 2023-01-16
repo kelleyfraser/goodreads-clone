be2e93bcfc5008ce782925ee059cd640f51f405e
Kelley Fraser <kelleyirwinfraser@gmail.com>
2023-01-16 09:38:25 +0000

    Build announcements component.

---
+++ src/components/announcement.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Announcement() {
+  return (
+    <div className="section main-panel announcement-section">
+      <div className="announcement-container">
+        <img src="./images/announcement.png"></img>
+        <div className="announcement-text">
+          <h2>Want to read more in 2023?</h2>
+          <h3>Tell us how many books you aim to finish this year and we'll help you stay on track!</h3>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Announcement;
